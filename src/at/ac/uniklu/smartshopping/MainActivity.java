c0a29fa36ee15b572bc2ba4a5340f618b6e35bc4
PC-Lab <PC-Lab@uni-klu>
2014-11-26 20:43:21 +0100

    Some change.

--- src/at/ac/uniklu/smartshopping/MainActivity.java
+++ src/at/ac/uniklu/smartshopping/MainActivity.java
@@ -330,6 +330,8 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	
+	
 	private void connectToServerAndSendString() {
 		try {
 //			Method m=device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
